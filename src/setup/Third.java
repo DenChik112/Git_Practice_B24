6aac0b88a2a211ffe52ed34f6a493cd9e21651db
DenysYechkalov <yechkalovdenys@gmail.com>
2021-10-20 15:30:41 -0400

    added file third

---
+++ src/setup/Third.java
@@ -0,0 +1,11 @@
+package setup;
+
+public class Third {
+    public static void main(String[] args) {
+        System.out.println("5");
+        m();
+    }
+    public  static void m (){
+        System.out.println("6");
+    }
+}
