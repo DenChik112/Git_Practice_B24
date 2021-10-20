4a9e1c0f69e81ed912fc63016e52f84970993189
DenysYechkalov <yechkalovdenys@gmail.com>
2021-10-20 17:20:24 -0400

    wooden spoon added

---
+++ src/setup/Tests.java
@@ -0,0 +1,10 @@
+package setup;
+
+public class Tests {
+    public static void main(String[] args) {
+        for(int i =0;i<5;i++){
+            System.out.println("Wooden spoon");
+        }
+
+    }
+}
